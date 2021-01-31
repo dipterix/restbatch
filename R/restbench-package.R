493fa33e6a343773571105c88ca9661b0173ec0e
dipterix <zw23@rice.edu>
2021-01-31 15:39:35 -0600

    removed c code

--- R/restbench-package.R
+++ R/restbench-package.R
@@ -1,9 +1,3 @@
 ## usethis namespace: start
-#' @useDynLib restbench, .registration = TRUE
 ## usethis namespace: end
 NULL
-
-
-.onUnload <- function (libpath) {
-  library.dynam.unload("restbench", libpath)
-}
